8d0f8c367e2d15fc7e85a2a2b511eb516cd7ed17
Papa bear <alexinbuda@gmail.com>
2025-08-21 15:03:37 -0400

    Lab 2 completed

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,9 +7,6 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-const giftRoutes = require('./routes/giftRoutes');
-
-
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -23,15 +20,12 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
-
-
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
 
-
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/giftRoutes')
 
 
 const pinoHttp = require('pino-http');
@@ -44,7 +38,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes)
 
 
 // Global Error Handler
