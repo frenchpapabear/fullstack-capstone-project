2e0ed3bd07190e3842e76035ca5a60ec21fada1b
Papa bear <alexinbuda@gmail.com>
2025-08-21 14:42:40 -0400

    lab1 completed

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,8 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+const giftRoutes = require('./routes/giftRoutes');
+
 
 const app = express();
 app.use("*",cors());
@@ -21,9 +23,12 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
+
+
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
