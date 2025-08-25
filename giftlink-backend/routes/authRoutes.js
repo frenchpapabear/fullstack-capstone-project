4400b453fc2fe1cc72f1b3bb5cb6663ecf25fba4
frenchpapabear <notrepapabear@gmail.com>
2025-08-25 17:27:39 +0200

    Lint Error Fix

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-//Step 1 - Task 2: Import necessary packages
+/*jshint esversion: 8 */
 const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
-const pino = require('pino');  // Import Pino logger
+const pino = require('pino');  
 
 //Step 1 - Task 3: Create a Pino logger instance
 const logger = pino();  // Create a Pino logger instance
@@ -19,7 +19,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
-        const db = await connectToDatabase()
+        const db = await connectToDatabase();
 
         // Task 2: Access MongoDB collection
         const collection = db.collection("users");
@@ -59,14 +59,14 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
-        const db = await connectToDatabase()
+        const db = await connectToDatabase();
         // Task 2: Access MongoDB `users` collection
         const collection = db.collection("users");
         // Task 3: Check for user credentials in database
         const theUser = await collection.findOne({ email: req.body.email });
         // Task 4: Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
         if (theUser) {
-            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
             if(!result) {
                 logger.error('Passwords do not match');
                 return res.status(404).json({ error: 'Wrong pasword' });
